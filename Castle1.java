8a92e22a6d9b88f52c3bb0303ea1a0e89191d933
nick497 <55507778+nick497@users.noreply.github.com>
2026-02-05 21:00:18 -0500

    Added some more map parts of Castle1

--- Castle1.java
+++ Castle1.java
@@ -1,27 +1,14 @@
 public class Castle1 {
-    public static void main(String[] args) {
-
-    }
-    public static void castle1_W10()
+    public static void castle1_W9()
     {
         Map_Methods.clearScreen(); //Clears the screen
-        Map_Methods.resetCastle1(); //Gets rid of other positions for the player on map
-        Map_Methods.currentMap = "castle1_W10"; //Sets map for ifStatements.
+        Map_Methods.resetCastle1(); //Gets rid of other positions for the player on the map
+        Map_Methods.currentMap = "castle1_W9"; //Sets map for ifStatements
         Map_Methods.classMap = "castle1"; //Sets the current class map for the if statements
-        Map_Methods.castle1_7 = "#o...............#"; //Sets position on map.
+        Map_Methods.castle1_7 = "#o...............#"; //Sets position on the map
         Map_Methods.castle1Map(); //Prints the map
         ifStatements.runGameLoop();
         //No need to call Map_Methods.healthandCommands(), already called by runGameLoop.
-    }
-    public static void castle1_W9()
-    {
-        Map_Methods.clearScreen();
-        Map_Methods.resetCastle1();
-        Map_Methods.currentMap = "castle1_W9";
-        Map_Methods.classMap = "castle1";
-        Map_Methods.castle1_7 = "#.o..............#"; 
-        Map_Methods.castle1Map();
-        ifStatements.runGameLoop();
     }
     public static void castle1_W8()
     {
@@ -29,7 +16,7 @@ public class Castle1 {
         Map_Methods.resetCastle1();
         Map_Methods.currentMap = "castle1_W8";
         Map_Methods.classMap = "castle1";
-        Map_Methods.castle1_7 = "#..o.............#";
+        Map_Methods.castle1_7 = "#.o..............#";
         Map_Methods.castle1Map();
         ifStatements.runGameLoop();
     }
@@ -39,7 +26,7 @@ public class Castle1 {
         Map_Methods.resetCastle1();
         Map_Methods.currentMap = "castle1_W7";
         Map_Methods.classMap = "castle1";
-        Map_Methods.castle1_7 = "#...o............#";
+        Map_Methods.castle1_7 = "#..o.............#";
         Map_Methods.castle1Map();
         ifStatements.runGameLoop();
     }
@@ -49,7 +36,7 @@ public class Castle1 {
         Map_Methods.resetCastle1();
         Map_Methods.currentMap = "castle1_W6";
         Map_Methods.classMap = "castle1";
-        Map_Methods.castle1_7 = "#....o...........#";
+        Map_Methods.castle1_7 = "#...o............#";
         Map_Methods.castle1Map();
         ifStatements.runGameLoop();
     }
@@ -59,7 +46,7 @@ public class Castle1 {
         Map_Methods.resetCastle1();
         Map_Methods.currentMap = "castle1_W5";
         Map_Methods.classMap = "castle1";
-        Map_Methods.castle1_7 = "#.....o..........#";
+        Map_Methods.castle1_7 = "#....o...........#";
         Map_Methods.castle1Map();
         ifStatements.runGameLoop();
     }
@@ -69,7 +56,7 @@ public class Castle1 {
         Map_Methods.resetCastle1();
         Map_Methods.currentMap = "castle1_W4";
         Map_Methods.classMap = "castle1";
-        Map_Methods.castle1_7 = "#......o.........#";
+        Map_Methods.castle1_7 = "#.....o..........#";
         Map_Methods.castle1Map();
         ifStatements.runGameLoop();
     }
@@ -79,7 +66,7 @@ public class Castle1 {
         Map_Methods.resetCastle1();
         Map_Methods.currentMap = "castle1_W3";
         Map_Methods.classMap = "castle1";
-        Map_Methods.castle1_7 = "#.......o........#";
+        Map_Methods.castle1_7 = "#......o.........#";
         Map_Methods.castle1Map();
         ifStatements.runGameLoop();
     }
@@ -89,7 +76,7 @@ public class Castle1 {
         Map_Methods.resetCastle1();
         Map_Methods.currentMap = "castle1_W2";
         Map_Methods.classMap = "castle1";
-        Map_Methods.castle1_7 = "#........o.......#";
+        Map_Methods.castle1_7 = "#.......o........#";
         Map_Methods.castle1Map();
         ifStatements.runGameLoop();
     }
@@ -99,7 +86,7 @@ public class Castle1 {
         Map_Methods.resetCastle1();
         Map_Methods.currentMap = "castle1_W1";
         Map_Methods.classMap = "castle1";
-        Map_Methods.castle1_7 = "#.........o......#";
+        Map_Methods.castle1_7 = "#........o.......#";
         Map_Methods.castle1Map();
         ifStatements.runGameLoop();
     }
@@ -109,7 +96,67 @@ public class Castle1 {
         Map_Methods.resetCastle1();
         Map_Methods.currentMap = "castle1";
         Map_Methods.classMap = "castle1";
-        Map_Methods.castle1_7 = "#..........o.....#";
+        Map_Methods.castle1_7 = "#.........o......#";
+        Map_Methods.castle1Map();
+        ifStatements.runGameLoop();
+    }
+    public static void castle1_E1()
+    {
+        Map_Methods.clearScreen();
+        Map_Methods.resetCastle1();
+        Map_Methods.currentMap = "castle1_E1";
+        Map_Methods.classMap = "castle1";
+        Map_Methods.castle1_7 = "#..........o.....#"; 
+        Map_Methods.castle1Map();
+        ifStatements.runGameLoop();
+    }
+    public static void castle1_E2()
+    {
+        Map_Methods.clearScreen();
+        Map_Methods.resetCastle1();
+        Map_Methods.currentMap = "castle1_E2";
+        Map_Methods.classMap = "castle1";
+        Map_Methods.castle1_7 = "#...........o....#"; 
+        Map_Methods.castle1Map();
+        ifStatements.runGameLoop();
+    }
+    public static void castle1_E3()
+    {
+        Map_Methods.clearScreen();
+        Map_Methods.resetCastle1();
+        Map_Methods.currentMap = "castle1_E3";
+        Map_Methods.classMap = "castle1";
+        Map_Methods.castle1_7 = "#............o...#"; 
+        Map_Methods.castle1Map();
+        ifStatements.runGameLoop();
+    }
+    public static void castle1_E4()
+    {
+        Map_Methods.clearScreen();
+        Map_Methods.resetCastle1();
+        Map_Methods.currentMap = "castle1_E4";
+        Map_Methods.classMap = "castle1";
+        Map_Methods.castle1_7 = "#.............o..#"; 
+        Map_Methods.castle1Map();
+        ifStatements.runGameLoop();
+    }
+    public static void castle1_E5()
+    {
+        Map_Methods.clearScreen();
+        Map_Methods.resetCastle1();
+        Map_Methods.currentMap = "castle1_E5";
+        Map_Methods.classMap = "castle1";
+        Map_Methods.castle1_7 = "#..............o.#"; 
+        Map_Methods.castle1Map();
+        ifStatements.runGameLoop();
+    }
+    public static void castle1_E6()
+    {
+        Map_Methods.clearScreen();
+        Map_Methods.resetCastle1();
+        Map_Methods.currentMap = "castle1_E6";
+        Map_Methods.classMap = "castle1";
+        Map_Methods.castle1_7 = "#...............o#"; 
         Map_Methods.castle1Map();
         ifStatements.runGameLoop();
     }
